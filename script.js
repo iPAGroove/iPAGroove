1598424fd9a8accf097f51a2699b8e7aa0f44a4a
iPAGroove <ipagroove@gmail.com>
2025-06-03 15:10:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,15 +4,27 @@ fetch('games.json')
   .then(res => res.json())
   .then(data => {
     allGames = data;
+    renderTop(data);
     renderList(data);
   });
 
+const topList = document.getElementById('topList');
 const gamesList = document.getElementById('gamesList');
 const searchInput = document.getElementById('searchInput');
 const genreFilter = document.getElementById('genreFilter');
 
 searchInput.addEventListener('input', filterGames);
 genreFilter.addEventListener('change', filterGames);
+
+function createCard(game) {
+  return `
+    <div class="min-w-[140px] bg-white/10 rounded-xl p-3 flex flex-col items-center">
+      <img src="${game.icon}" alt="${game.name}" class="w-16 h-16 rounded-xl mb-2" />
+      <h3 class="text-center font-semibold text-sm">${game.name}</h3>
+      <a href="${game.download}" class="mt-auto text-blue-400 underline text-xs">Скачать</a>
+    </div>
+  `;
+}
 
 function createTile(game) {
   return `
@@ -25,6 +37,11 @@ function createTile(game) {
       <a href="${game.download}" class="text-blue-400 underline text-sm flex-shrink-0">Скачать</a>
     </div>
   `;
+}
+
+function renderTop(games) {
+  const topGames = games.filter(g => g.top);
+  topList.innerHTML = topGames.map(createCard).join('');
 }
 
 function renderList(games) {
@@ -40,5 +57,6 @@ function filterGames() {
     (genre === "" || g.genre === genre)
   );
 
+  renderTop(filtered);
   renderList(filtered);
 }
